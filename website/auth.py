c3b18921a114bda118618d0e9ffd2abcc644b4de
Daniil <danokp@mail.ru>
2023-03-21 15:59:00 +0700

    Add function to add a new user to database

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,27 @@
-from flask import Blueprint
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for
+)
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('login')
+
+@auth.route('login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    data = request.form
+    print(data)
+    return render_template(
+        'login.html',
+        text='Testing'
+    )
 
 
 @auth.route('logout')
@@ -13,6 +29,30 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('sign-up')
+@auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 5:
+            flash('Email have to be longer than 4 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('Email have to be longer than 1 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 8:
+            flash('Password have to be longer than 7 characters.', category='error')
+        else:
+            new_user = User(
+                email=email,
+                first_name=first_name,
+                password=generate_password_hash(password1, method='sha256')
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+    return render_template('sign_up.html')
